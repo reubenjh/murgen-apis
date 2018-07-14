1e644d30bf0acb359f3ec2a680ce4e82ac34ab11
catepalmer <catherinepalmer@gmail.com>
2018-07-14 12:03:50 +1200

    set up boilerp[late

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,21 +6,27 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      fruits: []
+      fruits: ['potatoes', 'apples', 'oranges']
     }
   }
 
   componentDidMount () {
     getFruits()
       .then(fruits => {
-        this.setState({fruits})
+        let newFruits = fruits
+        newFruits.push(this.state.fruits[0])
+        newFruits.push(this.state.fruits[1])
+        newFruits.push(this.state.fruits[2])
+        this.setState({
+          fruits: newFruits
+        })
       })
   }
 
   render () {
     return (
       <div className='app'>
-        <h1>Fullstack Boilerplate</h1>
+        <h1>icanhazRonSwansonhaiku'z</h1>
         <ul>
           {this.state.fruits.map(fruit => (
             <li key={fruit}>{fruit}</li>
